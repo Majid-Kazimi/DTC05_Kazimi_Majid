feb0c967a85891e0dfafa3ece790745c7abf996b
Majid <mkazimi2@my.bcit.ca>
2024-10-15 13:39:19 -0700

    File modified-Oct-15-24

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -1,19 +1,28 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+// var firebaseConfig = {
+//     apiKey: ,
+//     authDomain: ,
+//     projectId: ,
+//     storageBucket: ,
+//     messagingSenderId: ,
+//     appId:
+// };
 
-//--------------------------------------------
-// initialize the Firebase app
-// initialize Firestore database if using it
-//--------------------------------------------
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const storage = firebase.storage();
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC8z_deQJHqhrNZSDBHmKH1w6LNDVpgWr4",
+//     authDomain: "dtc05-comp1800-demo.firebaseapp.com",
+//     projectId: "dtc05-comp1800-demo",
+//     storageBucket: "dtc05-comp1800-demo.appspot.com",
+//     messagingSenderId: "120931225139",
+//     appId: "1:120931225139:web:b37942dd96f7fc5bec1d46"
+// };
+
+// //--------------------------------------------
+// // initialize the Firebase app
+// // initialize Firestore database if using it
+// //--------------------------------------------
+// const app = firebase.initializeApp(firebaseConfig);
+// const db = firebase.firestore();
+// const storage = firebase.storage();
